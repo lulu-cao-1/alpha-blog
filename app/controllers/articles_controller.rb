8077b4418b9eda62acee4db71495112c69be2050
lulu-cao-1 <lcao@backstitch.io>
2024-06-15 12:39:56 -0500

    add list, new, create, edit, update, delete actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,45 @@ class ArticlesController < ApplicationController
     # byebug
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted successfully."
+    redirect_to articles_path
+  end
 end
