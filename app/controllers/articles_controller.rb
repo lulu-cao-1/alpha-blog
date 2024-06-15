3f2dbba41020e81383d65a8a9bb7554fbbc81f4d
lulu-cao-1 <lcao@backstitch.io>
2024-06-15 14:45:43 -0500

    refactor article mvc with partials for dry principle

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def show
     # byebug
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,12 +14,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
     # render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -29,8 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -39,9 +38,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was deleted successfully."
     redirect_to articles_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
