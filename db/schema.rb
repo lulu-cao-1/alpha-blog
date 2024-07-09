2b5f19bc70d4ba8d70effc2c387821ad77f95fd3
lulu-cao-1 <lcao@backstitch.io>
2024-07-08 22:07:24 -0500

    add password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_16_172044) do
+ActiveRecord::Schema.define(version: 2024_07_09_025000) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2024_06_16_172044) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
