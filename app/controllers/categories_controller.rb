1066097521c22847f4e33c72908eb5e7d2545774
lulu-cao-1 <lcao@backstitch.io>
2024-08-11 21:20:42 -0500

    add many-to-many association between articles and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def new
@@ -21,9 +24,30 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name was updated successfully."
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, article_ids: [])
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.is_admin?)
+      flash[:alert] = "Only admins can perform that action."
+      redirect_to categories_path
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_28_191457) do
+ActiveRecord::Schema.define(version: 2024_08_12_003922) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
